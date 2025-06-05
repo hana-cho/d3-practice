afb14cefe70a581b0dbfb385d28ef0408c974677
Hana Cho <hana-cho@users.noreply.github.com>
2025-06-04 22:08:08 -0700

    modified js, html, and css

--- d3-fruit-chart/script.js
+++ d3-fruit-chart/script.js
@@ -4,3 +4,37 @@ const data = [
     {fruit: "Papaya", count: 7},
     {fruit: "Avocado", count: 9},
 ];
+
+const svg = d3.select("svg");
+    const width = +svg.attr("width");
+    const height = +svg.attr("height");
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+    const x = d3.scaleBand()
+      .domain(data.map(d => d.fruit))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
+
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.count)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    svg.append("g")
+      .attr("fill", "steelblue")
+      .selectAll("rect")
+      .data(data)
+      .join("rect")
+        .attr("class", "bar")
+        .attr("x", d => x(d.fruit))
+        .attr("y", d => y(d.count))
+        .attr("height", d => y(0) - y(d.count))
+        .attr("width", x.bandwidth());
+
+    svg.append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+    svg.append("g")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
